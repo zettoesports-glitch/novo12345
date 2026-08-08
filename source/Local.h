bc10c57272828dca14591ff84123043c0ec98efe
zettoesports-glitch <zettoesports@gmail.com>
2026-08-08 20:25:43 -0300

    .

--- source/Local.h
+++
@@ -1,7 +0,0 @@
-// Local.h: interface for the Local
-//////////////////////////////////////////////////////////////////////
-#pragma once
-
-
-extern bool CheckSpecialText(char* pszText);
-extern bool CheckSpecialText(wchar_t* pszText);
